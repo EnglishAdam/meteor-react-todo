70633bceed77da42a5c2155d2ae77cb652cf50c0
Adam English <englishadam@hotmail.co.uk>
2019-05-08 22:29:05 +0100

    Added reactive tracker to app

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
- 
+import { withTracker } from 'meteor/react-meteor-data';
+
+// Component
 import Task from './Task.js';
+
+// Data
+import { Tasks } from '../api/tasks.js';
  
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
- 
+// export default class App extends Component {
+  // getTasks() {
+    //   return [
+      //     { _id: 1, text: 'This is task 1' },
+      //     { _id: 2, text: 'This is task 2' },
+      //     { _id: 3, text: 'This is task 3' },
+      //   ];
+      // }
+      
+      // renderTasks() {
+        //   return this.getTasks().map((task) => (
+          //     <Task key={task._id} task={task} />
+          //   ));
+          // }
+
+// App component - represents the whole app
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -32,3 +45,10 @@ export default class App extends Component {
     );
   }
 }
+
+// Track changes and add them to App props
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(App); // <-- Have wrapped App, now fetches the Tasks, do so rerenders when tasks change
