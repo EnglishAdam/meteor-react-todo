c46d4e407377b397398392ee0ec55ead4bea9a13
Adam English <englishadam@hotmail.co.uk>
2019-05-08 22:51:27 +0100

    Added incomplete count

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -69,7 +69,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -102,5 +102,6 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
   };
 })(App); // <-- Have wrapped App, now fetches the Tasks, do so rerenders when tasks change
